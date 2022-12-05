f1212841e8ff5332a0ee5212139cb2789b6d2eb1
rhinogroup <jorge@rhinocapitalgroup.com>
2022-12-05 18:47:33 -0500

    Create main.php
    This is the main php file which holds the html for the dropdown. Can also be .html

---
+++ main.php
@@ -0,0 +1,10 @@
+
+			<div class='mb-3 col-md-3 col-sm-12 p-2 mx-auto' id='bankid_div'>
+				<span class='p2plbl'>Country</span>
+				<div>
+					<input type="hidden" id='init_country' name='countryid'>
+					<input type='text' id='srch_country' class='form-control form-control-sm' placeholder='Start Typing...'>
+				</div>
+				<div class='col-12 d-none' id='cntryrst'></div>
+				<div class='error ef' id='bankid_err'></div>
+			</div>
