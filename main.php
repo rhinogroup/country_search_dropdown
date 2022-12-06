88dd79ef6978ead32b12e1d1b9558160e59cffd3
rhinogroup <jorge@rhinocapitalgroup.com>
2022-12-05 21:35:06 -0500

    Update main.php
    Added position absolute and relative to lines 4 and 8 to prevent the dropdown div from expanding the parent

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 
 			<div class='mb-3 col-md-3 col-sm-12 p-2 mx-auto' id='bankid_div'>
 				<span class='p2plbl'>Country</span>
-				<div>
+				<div style='position: relative;'>
 					<input type="hidden" id='init_country' name='countryid'>
-					<input type='text' id='srch_country' class='form-control form-control-sm' placeholder='Start Typing...'>
+					<input type='text' id='srch_country' class='form-control form-control-sm' placeholder='Start Typing...' style='z-index:1000;'>
 				</div>
-				<div class='col-12 d-none' id='cntryrst'></div>
+				<div class='col-12 d-none bg-light' id='cntryrst' style='position:absolute; top:4rem;'></div>
 				<div class='error ef' id='bankid_err'></div>
 			</div>
